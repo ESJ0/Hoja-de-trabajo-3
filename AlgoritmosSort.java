d94d629203db8a4ffbaf1c01f34319f36f76ae36
ESJ0 <usuarioj0s3p1@gmail.com>
2025-02-08 11:20:29 -0600

    Se creo la interfaz "AlgoritmosSort", la cual, debe implementar todos los sort que se implementen.

---
+++ AlgoritmosSort.java
@@ -0,0 +1,16 @@
+/**
+ * Se creo esta interfaz generica <T> para los algoritmos sort.
+ * Ademas se implementa "Comparable" para poder comparar elemntos para los metodos sort.
+ */
+
+interface AlgoritmosSort<T extends Comparable<T>> {
+    void sort(T[] array);
+}
+
+
+
+
+
+
+
+//hroughout Heaven and Earth, I alone am honored one.

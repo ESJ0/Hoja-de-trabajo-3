d2e23a364b93ce73e557e1efa9ab189b29b301d9
Oscar Rompich <oscarligo6@gmail.com>
2025-02-13 23:24:45 -0600

    Unit Tests para Radix y Selection Sort

--- UnitTests.java
+++ UnitTests.java
@@ -11,12 +11,16 @@ public class UnitTests {
     private QuickSort<Integer> quickSort;
     private Integer[] array;
     private Integer[] expected;
+    private SelectionSort<Integer> selectionSort;
+    private RadixSort <Integer> radixSort;
 
     @Before
     public void setUp() {
         mergeSort = new MergeSort<>();
         insertionSort = new InsertionSort<>();
         quickSort = new QuickSort<>(); 
+        selectionSort = new SelectionSort<>();
+        radixSort = new RadixSort<>();
         array = new Integer[]{ 6,5,8,7,2,1,3,10,9,4};
         expected = new Integer[]{1,2,3,4,5,6,7,8,9,10};
         
@@ -41,6 +45,22 @@ public class UnitTests {
     @Test
     public void shouldBeAbletoQuickSort() {
         quickSort.sort(array);
+        for (int i = 0; i < array.length; i++) {
+            assertEquals(array[i], expected[i]);
+        }
+    }
+
+    @Test 
+    public void shouldBeAbletoSelectSort() {
+        selectionSort.sort(array);
+        for (int i = 0; i < array.length; i++) {
+            assertEquals(array[i], expected[i]);
+        }
+    }
+
+    @Test
+    public void shouldBeAbleToRadixSort() {;
+        radixSort.sort(array);
         for (int i = 0; i < array.length; i++) {
             assertEquals(array[i], expected[i]);
         }
